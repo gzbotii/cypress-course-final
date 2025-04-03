4eeda4b053bfba759368694a0c3742c35eb4d62f
gzbotii <gzbotii@gmail.com>
2025-04-03 20:17:55 +0300

    Update NavItem.jsx

--- app/components/NavItem.jsx
+++ app/components/NavItem.jsx
@@ -1,9 +1,43 @@
-import Link from "next/link"
+import Link from "next/link";
 
-export default function NavItem({label,path,dataTest}){
-    return (
-        <Link data-test={dataTest} href={path}>
-            {label}
-        </Link>
-    )
+export default function NavItem({label, path, dataTest}) {
+  return (
+    <Link data-test={dataTest} href={path}>
+      {label}
+    </Link>
+  );
 }
+
+// function isLandscape(width, height) {
+//     if ( width > height) {
+//       console.log('landscpae')
+//     } else {
+//       console.log("portraite")
+//     }
+//   }
+
+//   isLandscape(23, 434);
+
+function checkSpeed(speed) {
+  speedLimit = 70;
+  if (speed < 70) {
+    console.log("ok");
+  } else if (speed > speedLimit && speed.Math) {
+    console.log("speed limit exceeded");
+  }
+}
+
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  const kmPenalty = 5;
+
+  if (speed < speedLimit + kmPenalty) {
+    console.log("ok");
+  } else {
+    const points = Math.floor((speed - speedLimit) / kmPenalty);
+    if (points >= 12) console.log("License suspended");
+    else console.log(`Points: ${points}`);
+  }
+}
+
+checkSpeed(75);
